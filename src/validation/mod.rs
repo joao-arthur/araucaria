857c8e7ede88325a470a8c6d96a5e393f8e70b2a
joao-arthur <joao.lothamer@gmail.com>
2025-03-24 20:32:22 -0300

    feat: value to string

--- src/validation/mod.rs
+++ src/validation/mod.rs
@@ -1,10 +1,14 @@
 use std::collections::HashMap;
 
 use bool::BoolValidation;
+use date::DateValidation;
+use datetime::DateTimeValidation;
+use email::EmailValidation;
 use num_f::NumFValidation;
 use num_i::NumIValidation;
 use num_u::NumUValidation;
 use str::StrValidation;
+use time::TimeValidation;
 
 pub mod bool;
 pub mod date;
@@ -24,10 +28,14 @@ pub struct ObjValidation {
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Validation {
-    Bool(BoolValidation),
     NumU(NumUValidation),
     NumI(NumIValidation),
     NumF(NumFValidation),
+    Bool(BoolValidation),
     Str(StrValidation),
+    Email(EmailValidation),
+    Date(DateValidation),
+    Time(TimeValidation),
+    DateTime(DateTimeValidation),
     Obj(ObjValidation),
 }
--- src/value/mod.rs
+++ src/value/mod.rs
@@ -6,10 +6,10 @@ pub mod stub;
 #[derive(Debug, PartialEq, Clone)]
 pub enum Value {
     None,
-    Bool(bool),
     NumU(u64),
     NumI(i64),
     NumF(f64),
+    Bool(bool),
     Str(String),
     Arr(Vec<Value>),
     Obj(HashMap<String, Value>),
@@ -57,16 +57,29 @@ impl<const N: usize> From<[(String, Value); N]> for Value {
     }
 }
 
+pub fn value_to_string(value: &Value) -> String {
+    match value {
+        Value::None => String::from(""),
+        Value::NumU(val) => val.to_string(),
+        Value::NumI(val) => val.to_string(),
+        Value::NumF(val) => val.to_string(),
+        Value::Bool(val) => val.to_string(),
+        Value::Str(val) => String::from("\"") + val + "\"",
+        Value::Arr(val) => String::from("[") + &val.iter().map(|v| value_to_string(v)).collect::<Vec<String>>().join(", ") + "]",
+        Value::Obj(val) => String::from("{ ") + &val.iter().map(|(k, v)| k.clone() + ": " + &value_to_string(v)).collect::<Vec<String>>().join(", ") +  " }",
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
     fn test_value_from() {
-        assert_eq!(Value::from(false), Value::Bool(false));
         assert_eq!(Value::from(8 as u64), Value::NumU(8));
         assert_eq!(Value::from(-3 as i64), Value::NumI(-3));
         assert_eq!(Value::from(-9.8), Value::NumF(-9.8));
+        assert_eq!(Value::from(false), Value::Bool(false));
         assert_eq!(Value::from("in vino veritas"), Value::Str(String::from("in vino veritas")));
         assert_eq!(
             Value::from([Value::from("veni"), Value::from("vidi"), Value::from("vici")]),
@@ -87,6 +100,44 @@ mod test {
                 (String::from("name"), Value::Str(String::from("Paul"))),
                 (String::from("alive"), Value::Bool(true)),
             ]))
+        );
+    }
+
+    #[test]
+    fn test_value_to_string() {
+        assert_eq!(value_to_string(&Value::None), String::from(""));
+        assert_eq!(value_to_string(&Value::NumU(4)), String::from("4"));
+        assert_eq!(value_to_string(&Value::NumI(-22)), String::from("-22"));
+        assert_eq!(value_to_string(&Value::NumF(-3.65)), String::from("-3.65"));
+        assert_eq!(value_to_string(&Value::Bool(true)), String::from("true"));
+        assert_eq!(value_to_string(&Value::from("Non sequitur")), String::from(r#""Non sequitur""#));
+        assert_eq!(value_to_string(
+            &Value::from([
+                Value::from("Ad nauseam"),
+                Value::from("Ad ignorantiam"),
+                Value::from([
+                    Value::from("Ad hominem"),
+                    Value::from("Ad verecundiam"),
+                ])
+            ])),
+            String::from(r#"["Ad nauseam", "Ad ignorantiam", ["Ad hominem", "Ad verecundiam"]]"#)
+        );
+        assert_eq!(value_to_string(
+            &Value::from([
+                (String::from("k_num"), Value::NumU(837)),
+                (String::from("k_bool"), Value::Bool(false)),
+                (String::from("k_str"), Value::from("Augustus")),
+                (String::from("k_nested"), Value::from([
+                    (String::from("l_1"), Value::from([
+                        (String::from("l_2"), Value::from([
+                            Value::from([
+                                (String::from("id"), Value::NumU(0))
+                            ])
+                        ]))
+                    ]))
+                ])),
+            ])),
+            String::from(r#"{ k_num: 837, k_bool: false, k_str: "Augustus", k_nested: { l_1: { l_2: [{ id: 0 }] } } }"#)
         );
     }
 }
