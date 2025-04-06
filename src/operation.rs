1db762e51e52a2b1e18134457d5c3c6f037db51d
joao-arthur <joao.lothamer@gmail.com>
2025-04-06 10:37:45 -0300

    feat(operation): compare

--- src/operation.rs
+++ src/operation.rs
@@ -43,8 +43,54 @@ where
     Ok(())
 }
 
+fn compare<T>(operation: &Operation<T>, value: &T) -> Result<(), Operation<T>>
+where
+    T: PartialOrd + Clone,
+{
+    match operation {
+        Operation::Eq(v) => {
+            if value != v {
+                return Err(Operation::Eq(v.clone()));
+            }
+        }
+        Operation::Ne(v) => {
+            if value == v {
+                return Err(Operation::Ne(v.clone()));
+            }
+        }
+        Operation::Gt(v) => {
+            if value <= v {
+                return Err(Operation::Gt(v.clone()));
+            }
+        }
+        Operation::Lt(v) => {
+            if value >= v {
+                return Err(Operation::Lt(v.clone()));
+            }
+        }
+        Operation::Ge(v) => {
+            if value < v {
+                return Err(Operation::Ge(v.clone()));
+            }
+        }
+        Operation::Le(v) => {
+            if value > v {
+                return Err(Operation::Le(v.clone()));
+            }
+        }
+        Operation::Btwn(v_a, v_b) => {
+            if value < v_a || value > v_b {
+                return Err(Operation::Btwn(v_a.clone(), v_b.clone()));
+            }
+        }
+    }
+    Ok(())
+}
+
 #[cfg(test)]
 mod test {
+    use crate::operation::compare;
+
     use super::{compare_eq, Operation, OperationEq};
 
     #[test]
@@ -121,5 +167,81 @@ mod test {
     fn test_compare_eq_usize_err() {
         assert_eq!(compare_eq(&OperationEq::Eq(10 as usize), &8), Err(OperationEq::Eq(10)));
         assert_eq!(compare_eq(&OperationEq::Ne(10 as usize), &10), Err(OperationEq::Ne(10)));
+    }
+
+    #[test]
+    fn test_compare_boolean_ok() {
+        assert_eq!(compare(&Operation::Eq(false), &false), Ok(()));
+        assert_eq!(compare(&Operation::Eq(true), &true), Ok(()));
+        assert_eq!(compare(&Operation::Ne(false), &true), Ok(()));
+        assert_eq!(compare(&Operation::Ne(true), &false), Ok(()));
+    }
+
+    #[test]
+    fn test_compare_boolean_err() {
+        assert_eq!(compare(&Operation::Eq(false), &true), Err(Operation::Eq(false)));
+        assert_eq!(compare(&Operation::Eq(true), &false), Err(Operation::Eq(true)));
+        assert_eq!(compare(&Operation::Ne(false), &false), Err(Operation::Ne(false)));
+        assert_eq!(compare(&Operation::Ne(true), &true), Err(Operation::Ne(true)));
+    }
+
+    #[test]
+    fn test_compare_str_ok() {
+        assert_eq!(compare(&Operation::Eq(String::from("swords")), &String::from("swords")), Ok(()));
+        assert_eq!(compare(&Operation::Ne(String::from("swords")), &String::from("sandals")), Ok(()));
+    }
+
+    #[test]
+    fn test_compare_str_err() {
+        assert_eq!(compare(&Operation::Eq(String::from("swords")), &String::from("sandals")), Err(Operation::Eq(String::from("swords"))));
+        assert_eq!(compare(&Operation::Ne(String::from("swords")), &String::from("swords")), Err(Operation::Ne(String::from("swords"))));
+    }
+
+    #[test]
+    fn test_compare_u64_ok() {
+        assert_eq!(compare(&Operation::Eq(10 as u64), &10), Ok(()));
+        assert_eq!(compare(&Operation::Ne(10 as u64), &42), Ok(()));
+    }
+
+    #[test]
+    fn test_compare_u64_err() {
+        assert_eq!(compare(&Operation::Eq(10 as u64), &8), Err(Operation::Eq(10 as u64)));
+        assert_eq!(compare(&Operation::Ne(10 as u64), &10), Err(Operation::Ne(10 as u64)));
+    }
+
+    #[test]
+    fn test_compare_i64_ok() {
+        assert_eq!(compare(&Operation::Eq(-10 as i64), &-10), Ok(()));
+        assert_eq!(compare(&Operation::Ne(-10 as i64), &-42), Ok(()));
+    }
+
+    #[test]
+    fn test_compare_i64_err() {
+        assert_eq!(compare(&Operation::Eq(-10 as i64), &8), Err(Operation::Eq(-10 as i64)));
+        assert_eq!(compare(&Operation::Ne(-10 as i64), &-10), Err(Operation::Ne(-10 as i64)));
+    }
+
+    #[test]
+    fn test_compare_f64_ok() {
+        assert_eq!(compare(&Operation::Eq(-10.5 as f64), &-10.5), Ok(()));
+        assert_eq!(compare(&Operation::Ne(-10.5 as f64), &-42.5), Ok(()));
+    }
+
+    #[test]
+    fn test_compare_f64_err() {
+        assert_eq!(compare(&Operation::Eq(-10.5 as f64), &-4.25), Err(Operation::Eq(-10.5 as f64)));
+        assert_eq!(compare(&Operation::Ne(-10.5 as f64), &-10.5), Err(Operation::Ne(-10.5 as f64)));
+    }
+
+    #[test]
+    fn test_compare_usize_ok() {
+        assert_eq!(compare(&Operation::Eq(10 as usize), &10), Ok(()));
+        assert_eq!(compare(&Operation::Ne(10 as usize), &42), Ok(()));
+    }
+
+    #[test]
+    fn test_compare_usize_err() {
+        assert_eq!(compare(&Operation::Eq(10 as usize), &8), Err(Operation::Eq(10)));
+        assert_eq!(compare(&Operation::Ne(10 as usize), &10), Err(Operation::Ne(10)));
     }
 }
