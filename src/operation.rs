d5a520bc226a76ab3e31c308c682ff0f361e40c0
joao-arthur <joao.lothamer@gmail.com>
2025-04-06 12:33:50 -0300

    test(operation): compare

--- src/operation.rs
+++ src/operation.rs
@@ -18,13 +18,13 @@ pub enum Operation<T> {
     Eq(T),
     Ne(T),
     Gt(T),
-    Lt(T),
     Ge(T),
+    Lt(T),
     Le(T),
     Btwn(T, T),
 }
 
-fn compare_eq<T>(operation: &OperationEq<T>, value: &T) -> Result<(), OperationEq<T>>
+pub fn compare_eq<T>(operation: &OperationEq<T>, value: &T) -> Result<(), OperationEq<T>>
 where
     T: PartialEq + Clone,
 {
@@ -43,7 +43,7 @@ where
     Ok(())
 }
 
-fn compare<T>(operation: &Operation<T>, value: &T) -> Result<(), Operation<T>>
+pub fn compare<T>(operation: &Operation<T>, value: &T) -> Result<(), Operation<T>>
 where
     T: PartialOrd + Clone,
 {
@@ -63,14 +63,14 @@ where
                 return Err(Operation::Gt(v.clone()));
             }
         }
-        Operation::Lt(v) => {
-            if value >= v {
-                return Err(Operation::Lt(v.clone()));
-            }
-        }
         Operation::Ge(v) => {
             if value < v {
                 return Err(Operation::Ge(v.clone()));
+            }
+        }
+        Operation::Lt(v) => {
+            if value >= v {
+                return Err(Operation::Lt(v.clone()));
             }
         }
         Operation::Le(v) => {
@@ -123,50 +123,58 @@ mod test {
 
     #[test]
     fn test_compare_eq_u64_ok() {
-        assert_eq!(compare_eq(&OperationEq::Eq(10 as u64), &10), Ok(()));
-        assert_eq!(compare_eq(&OperationEq::Ne(10 as u64), &42), Ok(()));
+        let v: u64 = 10;
+        assert_eq!(compare_eq(&OperationEq::Eq(v), &10), Ok(()));
+        assert_eq!(compare_eq(&OperationEq::Ne(v), &42), Ok(()));
     }
 
     #[test]
     fn test_compare_eq_u64_err() {
-        assert_eq!(compare_eq(&OperationEq::Eq(10 as u64), &8), Err(OperationEq::Eq(10 as u64)));
-        assert_eq!(compare_eq(&OperationEq::Ne(10 as u64), &10), Err(OperationEq::Ne(10 as u64)));
+        let v: u64 = 10;
+        assert_eq!(compare_eq(&OperationEq::Eq(v), &8), Err(OperationEq::Eq(v)));
+        assert_eq!(compare_eq(&OperationEq::Ne(v), &10), Err(OperationEq::Ne(v)));
     }
 
     #[test]
     fn test_compare_eq_i64_ok() {
-        assert_eq!(compare_eq(&OperationEq::Eq(-10 as i64), &-10), Ok(()));
-        assert_eq!(compare_eq(&OperationEq::Ne(-10 as i64), &-42), Ok(()));
+        let v: i64 = -10;
+        assert_eq!(compare_eq(&OperationEq::Eq(v), &-10), Ok(()));
+        assert_eq!(compare_eq(&OperationEq::Ne(v), &-42), Ok(()));
     }
 
     #[test]
     fn test_compare_eq_i64_err() {
-        assert_eq!(compare_eq(&OperationEq::Eq(-10 as i64), &8), Err(OperationEq::Eq(-10 as i64)));
-        assert_eq!(compare_eq(&OperationEq::Ne(-10 as i64), &-10), Err(OperationEq::Ne(-10 as i64)));
+        let v: i64 = -10;
+        assert_eq!(compare_eq(&OperationEq::Eq(v), &8), Err(OperationEq::Eq(v)));
+        assert_eq!(compare_eq(&OperationEq::Ne(v), &-10), Err(OperationEq::Ne(v)));
     }
 
     #[test]
     fn test_compare_eq_f64_ok() {
-        assert_eq!(compare_eq(&OperationEq::Eq(-10.5 as f64), &-10.5), Ok(()));
-        assert_eq!(compare_eq(&OperationEq::Ne(-10.5 as f64), &-42.5), Ok(()));
+        let v: f64 = -10.5;
+        assert_eq!(compare_eq(&OperationEq::Eq(v), &-10.5), Ok(()));
+        assert_eq!(compare_eq(&OperationEq::Ne(v), &-42.5), Ok(()));
     }
 
     #[test]
     fn test_compare_eq_f64_err() {
-        assert_eq!(compare_eq(&OperationEq::Eq(-10.5 as f64), &-4.25), Err(OperationEq::Eq(-10.5 as f64)));
-        assert_eq!(compare_eq(&OperationEq::Ne(-10.5 as f64), &-10.5), Err(OperationEq::Ne(-10.5 as f64)));
+        let v: f64 = -10.5;
+        assert_eq!(compare_eq(&OperationEq::Eq(v), &-4.25), Err(OperationEq::Eq(v)));
+        assert_eq!(compare_eq(&OperationEq::Ne(v), &-10.5), Err(OperationEq::Ne(v)));
     }
 
     #[test]
     fn test_compare_eq_usize_ok() {
-        assert_eq!(compare_eq(&OperationEq::Eq(10 as usize), &10), Ok(()));
-        assert_eq!(compare_eq(&OperationEq::Ne(10 as usize), &42), Ok(()));
+        let v: usize = 10;
+        assert_eq!(compare_eq(&OperationEq::Eq(v), &10), Ok(()));
+        assert_eq!(compare_eq(&OperationEq::Ne(v), &42), Ok(()));
     }
 
     #[test]
     fn test_compare_eq_usize_err() {
-        assert_eq!(compare_eq(&OperationEq::Eq(10 as usize), &8), Err(OperationEq::Eq(10)));
-        assert_eq!(compare_eq(&OperationEq::Ne(10 as usize), &10), Err(OperationEq::Ne(10)));
+        let v: usize = 10;
+        assert_eq!(compare_eq(&OperationEq::Eq(v), &8), Err(OperationEq::Eq(v)));
+        assert_eq!(compare_eq(&OperationEq::Ne(v), &10), Err(OperationEq::Ne(v)));
     }
 
     #[test]
@@ -198,50 +206,250 @@ mod test {
     }
 
     #[test]
-    fn test_compare_u64_ok() {
-        assert_eq!(compare(&Operation::Eq(10 as u64), &10), Ok(()));
-        assert_eq!(compare(&Operation::Ne(10 as u64), &42), Ok(()));
+    fn test_compare_u64_eq() {
+        let v: u64 = 10;
+        assert_eq!(compare(&Operation::Eq(v), &9), Err(Operation::Eq(v)));
+        assert_eq!(compare(&Operation::Eq(v), &10), Ok(()));
+        assert_eq!(compare(&Operation::Eq(v), &11), Err(Operation::Eq(v)));
     }
 
     #[test]
-    fn test_compare_u64_err() {
-        assert_eq!(compare(&Operation::Eq(10 as u64), &8), Err(Operation::Eq(10 as u64)));
-        assert_eq!(compare(&Operation::Ne(10 as u64), &10), Err(Operation::Ne(10 as u64)));
+    fn test_compare_u64_ne() {
+        let v: u64 = 10;
+        assert_eq!(compare(&Operation::Ne(v), &9), Ok(()));
+        assert_eq!(compare(&Operation::Ne(v), &10), Err(Operation::Ne(v)));
+        assert_eq!(compare(&Operation::Ne(v), &11), Ok(()));
     }
 
     #[test]
-    fn test_compare_i64_ok() {
-        assert_eq!(compare(&Operation::Eq(-10 as i64), &-10), Ok(()));
-        assert_eq!(compare(&Operation::Ne(-10 as i64), &-42), Ok(()));
+    fn test_compare_u64_gt() {
+        let v: u64 = 10;
+        assert_eq!(compare(&Operation::Gt(v), &9), Err(Operation::Gt(v)));
+        assert_eq!(compare(&Operation::Gt(v), &10), Err(Operation::Gt(v)));
+        assert_eq!(compare(&Operation::Gt(v), &11), Ok(()));
+        assert_eq!(compare(&Operation::Gt(v), &12), Ok(()));
     }
 
     #[test]
-    fn test_compare_i64_err() {
-        assert_eq!(compare(&Operation::Eq(-10 as i64), &8), Err(Operation::Eq(-10 as i64)));
-        assert_eq!(compare(&Operation::Ne(-10 as i64), &-10), Err(Operation::Ne(-10 as i64)));
+    fn test_compare_u64_ge() {
+        let v: u64 = 10;
+        assert_eq!(compare(&Operation::Ge(v), &8), Err(Operation::Ge(v)));
+        assert_eq!(compare(&Operation::Ge(v), &9), Err(Operation::Ge(v)));
+        assert_eq!(compare(&Operation::Ge(v), &10), Ok(()));
+        assert_eq!(compare(&Operation::Ge(v), &11), Ok(()));
     }
 
     #[test]
-    fn test_compare_f64_ok() {
-        assert_eq!(compare(&Operation::Eq(-10.5 as f64), &-10.5), Ok(()));
-        assert_eq!(compare(&Operation::Ne(-10.5 as f64), &-42.5), Ok(()));
+    fn test_compare_u64_lt() {
+        let v: u64 = 10;
+        assert_eq!(compare(&Operation::Lt(v), &8), Ok(()));
+        assert_eq!(compare(&Operation::Lt(v), &9), Ok(()));
+        assert_eq!(compare(&Operation::Lt(v), &10), Err(Operation::Lt(v)));
+        assert_eq!(compare(&Operation::Lt(v), &11), Err(Operation::Lt(v)));
     }
 
     #[test]
-    fn test_compare_f64_err() {
-        assert_eq!(compare(&Operation::Eq(-10.5 as f64), &-4.25), Err(Operation::Eq(-10.5 as f64)));
-        assert_eq!(compare(&Operation::Ne(-10.5 as f64), &-10.5), Err(Operation::Ne(-10.5 as f64)));
+    fn test_compare_u64_le() {
+        let v: u64 = 10;
+        assert_eq!(compare(&Operation::Le(v), &9), Ok(()));
+        assert_eq!(compare(&Operation::Le(v), &10), Ok(()));
+        assert_eq!(compare(&Operation::Le(v), &11), Err(Operation::Le(v)));
+        assert_eq!(compare(&Operation::Le(v), &12), Err(Operation::Le(v)));
     }
 
     #[test]
-    fn test_compare_usize_ok() {
-        assert_eq!(compare(&Operation::Eq(10 as usize), &10), Ok(()));
-        assert_eq!(compare(&Operation::Ne(10 as usize), &42), Ok(()));
+    fn test_compare_u64_btwn() {
+        let a: u64 = 10;
+        let b: u64 = 100;
+        assert_eq!(compare(&Operation::Btwn(a, b), &9), Err(Operation::Btwn(a, b)));
+        assert_eq!(compare(&Operation::Btwn(a, b), &10), Ok(()));
+        assert_eq!(compare(&Operation::Btwn(a, b), &100), Ok(()));
+        assert_eq!(compare(&Operation::Btwn(a, b), &101), Err(Operation::Btwn(a, b)));
     }
 
     #[test]
-    fn test_compare_usize_err() {
-        assert_eq!(compare(&Operation::Eq(10 as usize), &8), Err(Operation::Eq(10)));
-        assert_eq!(compare(&Operation::Ne(10 as usize), &10), Err(Operation::Ne(10)));
+    fn test_compare_i64_eq() {
+        let v: i64 = -10;
+        assert_eq!(compare(&Operation::Eq(v), &-11), Err(Operation::Eq(v)));
+        assert_eq!(compare(&Operation::Eq(v), &-10), Ok(()));
+        assert_eq!(compare(&Operation::Eq(v), &-9), Err(Operation::Eq(v)));
+    }
+
+    #[test]
+    fn test_compare_i64_ne() {
+        let v: i64 = -10;
+        assert_eq!(compare(&Operation::Ne(v), &-11), Ok(()));
+        assert_eq!(compare(&Operation::Ne(v), &-10), Err(Operation::Ne(v)));
+        assert_eq!(compare(&Operation::Ne(v), &-9), Ok(()));
+    }
+
+    #[test]
+    fn test_compare_i64_gt() {
+        let v: i64 = -10;
+        assert_eq!(compare(&Operation::Gt(v), &-11), Err(Operation::Gt(v)));
+        assert_eq!(compare(&Operation::Gt(v), &-10), Err(Operation::Gt(v)));
+        assert_eq!(compare(&Operation::Gt(v), &-9), Ok(()));
+        assert_eq!(compare(&Operation::Gt(v), &-8), Ok(()));
+    }
+
+    #[test]
+    fn test_compare_i64_ge() {
+        let v: i64 = -10;
+        assert_eq!(compare(&Operation::Ge(v), &-12), Err(Operation::Ge(v)));
+        assert_eq!(compare(&Operation::Ge(v), &-11), Err(Operation::Ge(v)));
+        assert_eq!(compare(&Operation::Ge(v), &-10), Ok(()));
+        assert_eq!(compare(&Operation::Ge(v), &-9), Ok(()));
+    }
+
+    #[test]
+    fn test_compare_i64_lt() {
+        let v: i64 = -10;
+        assert_eq!(compare(&Operation::Lt(v), &-12), Ok(()));
+        assert_eq!(compare(&Operation::Lt(v), &-11), Ok(()));
+        assert_eq!(compare(&Operation::Lt(v), &-10), Err(Operation::Lt(v)));
+        assert_eq!(compare(&Operation::Lt(v), &-9), Err(Operation::Lt(v)));
+    }
+
+    #[test]
+    fn test_compare_i64_le() {
+        let v: i64 = -10;
+        assert_eq!(compare(&Operation::Le(v), &-11), Ok(()));
+        assert_eq!(compare(&Operation::Le(v), &-10), Ok(()));
+        assert_eq!(compare(&Operation::Le(v), &-9), Err(Operation::Le(v)));
+        assert_eq!(compare(&Operation::Le(v), &-8), Err(Operation::Le(v)));
+    }
+
+    #[test]
+    fn test_compare_i64_btwn() {
+        let a: i64 = -10;
+        let b: i64 = 10;
+        assert_eq!(compare(&Operation::Btwn(a, b), &-11), Err(Operation::Btwn(a, b)));
+        assert_eq!(compare(&Operation::Btwn(a, b), &-10), Ok(()));
+        assert_eq!(compare(&Operation::Btwn(a, b), &10), Ok(()));
+        assert_eq!(compare(&Operation::Btwn(a, b), &11), Err(Operation::Btwn(a, b)));
+    }
+
+    #[test]
+    fn test_compare_f64_eq() {
+        let v: f64 = -10.5;
+        assert_eq!(compare(&Operation::Eq(v), &-11.5), Err(Operation::Eq(v)));
+        assert_eq!(compare(&Operation::Eq(v), &-10.5), Ok(()));
+        assert_eq!(compare(&Operation::Eq(v), &-9.5), Err(Operation::Eq(v)));
+    }
+
+    #[test]
+    fn test_compare_f64_ne() {
+        let v: f64 = -10.5;
+        assert_eq!(compare(&Operation::Ne(v), &-11.5), Ok(()));
+        assert_eq!(compare(&Operation::Ne(v), &-10.5), Err(Operation::Ne(v)));
+        assert_eq!(compare(&Operation::Ne(v), &-9.5), Ok(()));
+    }
+
+    #[test]
+    fn test_compare_f64_gt() {
+        let v: f64 = -10.5;
+        assert_eq!(compare(&Operation::Gt(v), &-11.5), Err(Operation::Gt(v)));
+        assert_eq!(compare(&Operation::Gt(v), &-10.5), Err(Operation::Gt(v)));
+        assert_eq!(compare(&Operation::Gt(v), &-9.5), Ok(()));
+        assert_eq!(compare(&Operation::Gt(v), &-8.5), Ok(()));
+    }
+
+    #[test]
+    fn test_compare_f64_ge() {
+        let v: f64 = -10.5;
+        assert_eq!(compare(&Operation::Ge(v), &-12.5), Err(Operation::Ge(v)));
+        assert_eq!(compare(&Operation::Ge(v), &-11.5), Err(Operation::Ge(v)));
+        assert_eq!(compare(&Operation::Ge(v), &-10.5), Ok(()));
+        assert_eq!(compare(&Operation::Ge(v), &-9.5), Ok(()));
+    }
+
+    #[test]
+    fn test_compare_f64_lt() {
+        let v: f64 = -10.5;
+        assert_eq!(compare(&Operation::Lt(v), &-12.5), Ok(()));
+        assert_eq!(compare(&Operation::Lt(v), &-11.5), Ok(()));
+        assert_eq!(compare(&Operation::Lt(v), &-10.5), Err(Operation::Lt(v)));
+        assert_eq!(compare(&Operation::Lt(v), &-9.5), Err(Operation::Lt(v)));
+    }
+
+    #[test]
+    fn test_compare_f64_le() {
+        let v: f64 = -10.5;
+        assert_eq!(compare(&Operation::Le(v), &-11.5), Ok(()));
+        assert_eq!(compare(&Operation::Le(v), &-10.5), Ok(()));
+        assert_eq!(compare(&Operation::Le(v), &-9.5), Err(Operation::Le(v)));
+        assert_eq!(compare(&Operation::Le(v), &-8.5), Err(Operation::Le(v)));
+    }
+
+    #[test]
+    fn test_compare_f64_btwn() {
+        let a: f64 = -10.5;
+        let b: f64 = 10.5;
+        assert_eq!(compare(&Operation::Btwn(a, b), &-11.5), Err(Operation::Btwn(a, b)));
+        assert_eq!(compare(&Operation::Btwn(a, b), &-10.5), Ok(()));
+        assert_eq!(compare(&Operation::Btwn(a, b), &10.5), Ok(()));
+        assert_eq!(compare(&Operation::Btwn(a, b), &11.5), Err(Operation::Btwn(a, b)));
+    }
+
+    #[test]
+    fn test_compare_usize_eq() {
+        let v: usize = 10;
+        assert_eq!(compare(&Operation::Eq(v), &9), Err(Operation::Eq(v)));
+        assert_eq!(compare(&Operation::Eq(v), &10), Ok(()));
+        assert_eq!(compare(&Operation::Eq(v), &11), Err(Operation::Eq(v)));
+    }
+
+    #[test]
+    fn test_compare_usize_ne() {
+        let v: usize = 10;
+        assert_eq!(compare(&Operation::Ne(v), &9), Ok(()));
+        assert_eq!(compare(&Operation::Ne(v), &10), Err(Operation::Ne(v)));
+        assert_eq!(compare(&Operation::Ne(v), &11), Ok(()));
+    }
+
+    #[test]
+    fn test_compare_usize_gt() {
+        let v: usize = 10;
+        assert_eq!(compare(&Operation::Gt(v), &9), Err(Operation::Gt(v)));
+        assert_eq!(compare(&Operation::Gt(v), &10), Err(Operation::Gt(v)));
+        assert_eq!(compare(&Operation::Gt(v), &11), Ok(()));
+        assert_eq!(compare(&Operation::Gt(v), &12), Ok(()));
+    }
+
+    #[test]
+    fn test_compare_usize_ge() {
+        let v: usize = 10;
+        assert_eq!(compare(&Operation::Ge(v), &8), Err(Operation::Ge(v)));
+        assert_eq!(compare(&Operation::Ge(v), &9), Err(Operation::Ge(v)));
+        assert_eq!(compare(&Operation::Ge(v), &10), Ok(()));
+        assert_eq!(compare(&Operation::Ge(v), &11), Ok(()));
+    }
+
+    #[test]
+    fn test_compare_usize_lt() {
+        let v: usize = 10;
+        assert_eq!(compare(&Operation::Lt(v), &8), Ok(()));
+        assert_eq!(compare(&Operation::Lt(v), &9), Ok(()));
+        assert_eq!(compare(&Operation::Lt(v), &10), Err(Operation::Lt(v)));
+        assert_eq!(compare(&Operation::Lt(v), &11), Err(Operation::Lt(v)));
+    }
+
+    #[test]
+    fn test_compare_usize_le() {
+        let v: usize = 10;
+        assert_eq!(compare(&Operation::Le(v), &9), Ok(()));
+        assert_eq!(compare(&Operation::Le(v), &10), Ok(()));
+        assert_eq!(compare(&Operation::Le(v), &11), Err(Operation::Le(v)));
+        assert_eq!(compare(&Operation::Le(v), &12), Err(Operation::Le(v)));
+    }
+
+    #[test]
+    fn test_compare_usize_btwn() {
+        let a: usize = 10;
+        let b: usize = 100;
+        assert_eq!(compare(&Operation::Btwn(a, b), &9), Err(Operation::Btwn(a, b)));
+        assert_eq!(compare(&Operation::Btwn(a, b), &10), Ok(()));
+        assert_eq!(compare(&Operation::Btwn(a, b), &100), Ok(()));
+        assert_eq!(compare(&Operation::Btwn(a, b), &101), Err(Operation::Btwn(a, b)));
     }
 }
